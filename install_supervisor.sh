4002c0b77f454e8e13fab2d81f1657174086a8c1
xriv3r <xriv3r@outlook.com>
2019-01-22 11:47:14 +0800

    Update install_supervisor.sh

--- install_supervisor.sh
+++ install_supervisor.sh
@@ -1,7 +1,7 @@
 pip install supervisor
 
 cd /etc; wget https://raw.githubusercontent.com/xriv3r/OnePiece/master/supervisord.conf
-mkdir -p /etc/superviosr/
-cd /etc/superviosr/; wget https://raw.githubusercontent.com/xriv3r/OnePiece/master/sample_program.conf
+mkdir -p /etc/supervisor/
+cd /etc/supervisor/; wget https://raw.githubusercontent.com/xriv3r/OnePiece/master/sample_program.conf
 
 supervisord -c /etc/supervisord.conf
